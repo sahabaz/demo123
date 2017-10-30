c4a4c26d0b116512104e8e1d90d4f70b854b4525
admin <admin@qsp>
2017-10-30 20:27:13 +0530

    add

---
+++ selenium1/src/test/java/com/demo/program1.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class program1 
+{
+	@Test
+	public void m1()
+	{
+		Reporter.log("hiii" , true);
+	}
+	
+	
+}
